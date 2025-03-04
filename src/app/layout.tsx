c87fce4279756eb43b5ae2e557666f404c482d49
enosintech <enos.nsambaj@gmail.com>
2025-03-04 13:36:24 +0200

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { constructMetadata } from "@/lib/metadata";
 import { mont } from "@/lib/fonts";
 import Cursor from "@/components/cursor";
 import SmoothScroll from "@/components/smooth-scroll";
+import SiteBackground from "@/components/site-background";
 import { LoadingValueProvider } from "@/context/loadingValueContext";
 import { ViewTransitions } from "next-view-transitions"
 import Scroller from "@/components/scroller";
@@ -23,13 +24,13 @@ export default function RootLayout({
           className={`${mont.className} text-white bg-black antialiased`}
         >
           <LoadingValueProvider>
-            <SmoothScroll>
+            {/* <SmoothScroll> */}
               <Navbar />
               <Cursor />
               <Scroller />
-              {/* <SiteBackground /> */}
+              <SiteBackground />
               {children}
-            </SmoothScroll>
+            {/* </SmoothScroll> */}
           </LoadingValueProvider>
         </body>
       </html>
