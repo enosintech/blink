d5b9efa0d2c59946856cd51c5308db249146273f
enosintech <enos.nsambaj@gmail.com>
2025-04-23 23:12:21 +0200

    racks are crazy

--- src/components/passion-banner.tsx
+++ src/components/passion-banner.tsx
@@ -29,7 +29,7 @@ const PassionBanner = () => {
     <div className='px-10 sm:px-14 md:px-20 lg:px-24 xl:px-32 w-full flex flex-col items-center gap-20 text-center pt-32 pb-14'>
         <div className='flex flex-col items-center passion-banner translate-y-20 opacity-0'>
             <p className='text-lg md:text-xl xl:text-2xl font-extrabold'>FLAVOUR</p>
-            <p className='font-medium text-sm md:text-base xl:text-lg mt-2 max-w-[90%] md:max-w-[80%] xl:max-w-[70%] text-center'>Bold taste meets an iconic power up. Crafted for Earth's mightiest legends, and you.</p>
+            <p className='font-medium text-sm md:text-base xl:text-lg mt-2 max-w-[90%] md:max-w-[80%] xl:max-w-[70%] text-center'>Bold taste meets an iconic power up. Crafted for Earth&apos;s mightiest legends, and you.</p>
         </div>
         <div className='flex flex-col items-center passion-banner translate-y-20 opacity-0'>
             <p className='text-lg md:text-xl xl:text-2xl font-extrabold'>SEASONAL</p>
--- src/components/passion-explore.tsx
+++ src/components/passion-explore.tsx
@@ -8,7 +8,7 @@ const PassionExplore = () => {
   return (
     <div className='bg-gradient-to-b from-black via-black to-transparent rounded-t-xl pt-20 flex flex-col items-center px-10 sm:px-14 md:px-20 lg:px-24 xl:px-32'>
         <div className='w-full mt-5 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3'>
-          {redBullMotion.map((item, index, array) => (
+          {redBullMotion.map((item) => (
             <div
               key={item.id}
               className={`w-full h-[500px] sm:h-[800px] md:h-[500px] lg:h-[600px] xl:h-[450px] 2xl:h-[70vh] rounded-lg relative overflow-hidden`}
